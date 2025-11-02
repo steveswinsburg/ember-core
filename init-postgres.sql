10004250567c798ac5e90aa98c7406697c96d52a
Steve Swinsburg <steve.swinsburg@gmail.com>
2025-11-02 22:45:30 +1100

    Updates to startup and docker compose

--- init-postgres.sql
+++ init-postgres.sql
@@ -4,12 +4,12 @@
 CREATE USER IF NOT EXISTS fhir_app_user WITH PASSWORD 'password';
 
 -- Create FHIR schema and assign ownership to the application user
-CREATE SCHEMA IF NOT EXISTS fhir_data AUTHORIZATION fhir_app;
+CREATE SCHEMA IF NOT EXISTS fhir_data AUTHORIZATION fhir_app_user;
 
 -- Grant all necessary permissions to the application user on their schema
 GRANT ALL PRIVILEGES ON SCHEMA fhir_data TO fhir_app_user;
 
--- Set default privileges so future objects created in the schema belong to fhir_app
+-- Set default privileges so future objects created in the schema belong to fhir_app_user
 ALTER DEFAULT PRIVILEGES IN SCHEMA fhir_data GRANT ALL ON TABLES TO fhir_app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA fhir_data GRANT ALL ON SEQUENCES TO fhir_app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA fhir_data GRANT ALL ON FUNCTIONS TO fhir_app_user;
@@ -18,8 +18,8 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA fhir_data GRANT ALL ON FUNCTIONS TO fhir_app_
 -- CREATE SCHEMA IF NOT EXISTS another_schema AUTHORIZATION fhir_app_user;
 
 -- Grant permissions on additional schemas
-GRANT ALL PRIVILEGES ON SCHEMA another_schema TO fhir_app_user;
+-- GRANT ALL PRIVILEGES ON SCHEMA another_schema TO fhir_app_user;
 
 -- Set search path for the application user (optional but recommended)
-ALTER USER fhir_app SET search_path = fhir_data, public;
+ALTER USER fhir_app_user SET search_path = fhir_data, public;
 -- add additional schemas here if created
